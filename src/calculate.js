b722fc3237f382395c2e80f8c60200b676f98713
Roma8642 <froma5361@gmail.com>
2023-10-24 10:38:12 +0300

    calculate

---
+++ src/calculate.js
@@ -0,0 +1,11 @@
+const rate = 12
+const distance = 1200
+const price = 20
+function calculate (ratee,dist,pr){
+    let cost
+ 
+    cost = (ratee * pr)/100
+  
+return cost
+}
+console.log(calculate(rate,distance,price))
